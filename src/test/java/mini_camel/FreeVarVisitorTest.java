2489b440e85d4085f1142329dc67a0164a1b4e89
marssol <marssolina@gmail.com>
2015-01-08 15:41:54 +0100

    Implemented more unit tests for `FreeVarVisitor`.

--- src/test/java/mini_camel/FreeVarVisitorTest.java
+++ src/test/java/mini_camel/FreeVarVisitorTest.java
@@ -58,4 +58,56 @@ public class FreeVarVisitorTest {
                 "c"
         );
     }
+
+    @Test
+    public void test4() throws Exception {
+        assertFreeVars("let rec ack x y =\n" +
+                        "  if x <= 0 then y + 1 else\n" +
+                        "  if y <= 0 then ack (x - 1) 1 else\n" +
+                        "  ack (x - 1) (ack x (y - 1)) in ack 3 10"
+        );
+    }
+
+    @Test
+    public void test5() throws Exception {
+        assertFreeVars("let rec make_adder x =\n" +
+                        "  let rec adder y = x + y in\n" +
+                        "  adder in\n" +
+                        "make_adder 3"
+        );
+    }
+
+    @Test
+    public void test6() throws Exception {
+        assertFreeVars("let rec f x = x + 123 in\n" +
+                        "let rec g y = f in\n" +
+                        "g 456"
+        );
+    }
+
+    @Test
+    public void test7() throws Exception {
+        assertFreeVars("let x = (* bla *) 1 in 2"
+        );
+    }
+
+    @Test
+    public void test8() throws Exception {
+        assertFreeVars("if a then not (a >= b) else not d",
+                "a",
+                "b",
+                "d"
+        );
+    }
+
+
+    @Test
+    public void test9() throws Exception {
+        assertFreeVars(" if a+b then c else c-a",
+                "a",
+                "b",
+                "c"
+        );
+    }
+
 }
