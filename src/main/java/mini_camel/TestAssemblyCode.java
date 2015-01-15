b4965a544d30af863e324a1cdc2d1babf9e9f1b4
mommessc <clement73290@hotmail.fr>
2015-01-15 10:21:34 +0100

    Create AssemblyGenerator class

--- src/main/java/mini_camel/TestAssemblyCode.java
+++ src/main/java/mini_camel/TestAssemblyCode.java
@@ -1,18 +1,29 @@
 package mini_camel;
 
-import mini_camel.comp.MyCompiler;
+
+import mini_camel.comp.*;
 
 import java.io.*;
 
 public class TestAssemblyCode {
     static public void main(String argv[]) {
-        try {
-            MyCompiler c = new MyCompiler(new StringReader("print_newline"));
-            c.codeGeneration();
-            c.outputAssembly(new PrintStream("ARM/output.s"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+        Reader r;
+        //r = new StringReader("let x = 1 in print_newline");
+        r = new StringReader("let x = 1 in 5");
+
+        MyCompiler c = new MyCompiler(r);
+
+        c.parseCode();
+        c.preProcessCode();
+
+        System.out.println("starting code gen\n");
+        c.codeGeneration();
+
+        System.out.println("starting assembly gen\n");
+
+        PrintStream s = System.out;
+        c.outputAssembly(s);
     }
 }
 
