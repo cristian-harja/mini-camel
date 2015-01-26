bec7c9186fdc23fcb2b5540612b3a46dbdaff589
mommessc <clement73290@hotmail.fr>
2015-01-26 18:26:35 +0100

    Call and Booleans IR updated Fixed bugs with assembly generation

--- src/main/java/mini_camel/TestTransform.java
+++ src/main/java/mini_camel/TestTransform.java
@@ -16,10 +16,10 @@ public class TestTransform {
         try {
             Reader r;
             //r = new StringReader("let x = y in x + y");
-            //r = new StringReader("let y = 4 in let x = y in let z = print_int(x) in z");
+            r = new StringReader("let rec comp x y = if x = y then 1 else 0 in let a = comp true false in print_int(a)");
             //r = new StringReader("let x = 4 in let y = 12 in let z = x - y in z - 3 + 12");
             //r = new StringReader("let rec f x = let x = 1 in 4 in f(x)");
-            r = new StringReader("let t = 123 in let x = 12 in let a = true in let b = true in let s = 1 + if(b) then x else if not(a) = b then t else x + t in print_int(s)");
+            //r = new StringReader("let t = 123 in let x = 12 in let a = true in let b = true in let s = 1 + if(b) then x else if not(a) = b then t else x + t in print_int(s)");
 
 
 
